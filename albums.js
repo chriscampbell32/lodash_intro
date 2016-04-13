7442b1a20bfc4593a11bd60ca12df86d93b8b4e3
chriscampbell32 <chriscampbell32@yahoo.com>
2016-04-12 20:59:16 -0400

    groupBy is tight

--- albums.js
+++ albums.js
@@ -5,4 +5,12 @@ request("http://jsonplaceholder.typicode.com/albums")
     .then(function(response){
         var albums  = JSON.parse(response);
         var result = {};
-    })
+
+       var result = _.groupBy(albums, 'userId');
+       console.log(result);
+
+        // loop over array of objects
+            // check user_id and create new key if unique
+                // add album object to new hash
+        console.log(result[7]);
+    });
